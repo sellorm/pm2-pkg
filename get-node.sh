8d2023f2073880e511025e0bd2b898eaf94d1ab4
Mark Sellors <github@5vcc.com>
2017-03-12 00:10:27 +0000

    streamlined the number of build scripts

--- get-node.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl -o /tmp/node.tar.xz https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.xz
-
-tar -xvf /tmp/node.tar.xz -C ./slash/opt/pm2 --strip-components=1
-
-rm /tmp/node.tar.xz
-
