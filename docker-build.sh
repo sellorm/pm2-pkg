008bfdb5923695b2af1200a7be4729bf71fb6a47
Mark Sellors <github@5vcc.com>
2017-03-12 01:15:36 +0000

    removed pointless comment

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 BUILDOPTS=${1}
 
-# docker run -ti -v /Users/msellors/Documents/projects/pm2-pkg:/build --entrypoint "/build/buildpkgs" sellorm/build ${BUILDOPTS}
+# docker run
 docker run -ti -v ${PWD}:/build --entrypoint "/build/buildpkgs" sellorm/build ${BUILDOPTS}
